ebe8a65f5ac7e10c590b9e03c5d752d8719d93fb
suhongkim98 <kshz9815@naver.com>
2022-07-28 12:29:14 +0900

    refactor: add temp point table

--- src/main/java/com/devnus/belloga/point/point/domain/TempPoint.java
+++ src/main/java/com/devnus/belloga/point/point/domain/TempPoint.java
@@ -1,6 +1,5 @@
 package com.devnus.belloga.point.point.domain;
 
-
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,13 +7,13 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 /**
- * Point 엔티티이다.
+ * 라벨링 수행 시 받는 임시 포인트, 1 대 다
  */
 @Entity
-@Table(name = "point")
+@Table(name = "temp_point")
 @Getter
 @NoArgsConstructor
-public class Point {
+public class TempPoint {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,12 +22,16 @@ public class Point {
     @Column(name = "labeler_id")
     private String labelerId;
 
+    @Column(name = "labeling_uuid")
+    private String labelingUUID; // 라벨링 고유값
+
     @Column(name = "point_value")
     private Long pointValue;
 
     @Builder
-    public Point(String labelerId, Long pointValue) {
+    public TempPoint(String labelerId, String labelingUUID, Long pointValue) {
         this.labelerId = labelerId;
+        this.labelingUUID = labelingUUID;
         this.pointValue = pointValue;
     }
 }
