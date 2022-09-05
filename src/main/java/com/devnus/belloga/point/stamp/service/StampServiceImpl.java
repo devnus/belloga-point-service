74ac2b009765e2d6cdfcff4c97b61ac7042ce535
suhongkim98 <kshz9815@naver.com>
2022-09-05 11:25:44 +0900

    feat: implement external injection point ratio

--- src/main/java/com/devnus/belloga/point/stamp/service/StampServiceImpl.java
+++ src/main/java/com/devnus/belloga/point/stamp/service/StampServiceImpl.java
@@ -7,14 +7,24 @@ import com.devnus.belloga.point.point.repository.PointRepository;
 import com.devnus.belloga.point.stamp.domain.Stamp;
 import com.devnus.belloga.point.stamp.repository.StampRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 public class StampServiceImpl implements StampService {
     private final StampRepository stampRepository;
     private final PointRepository pointRepository;
+
+    private final long pointRatio; // application 설정파일에서 포인트 교환비율 설정
+    public StampServiceImpl(final StampRepository stampRepository,
+                            final PointRepository pointRepository,
+                            @Value("${app.stamp.point-ratio}")final long pointRatio) {
+
+        this.stampRepository = stampRepository;
+        this.pointRepository = pointRepository;
+        this.pointRatio = pointRatio;
+    }
 
     /**
      * 포인트를 소비해 스탬프를 찍는다
@@ -23,16 +33,14 @@ public class StampServiceImpl implements StampService {
     @Override
     @Transactional
     public void addStamp(String labelerId) {
-        Long ratio = 10L; // 교환비율은 상의해야함
-
         Point point = pointRepository.findByLabelerId(labelerId)
                 .orElseThrow(()->new NotFoundLabelerIdException());
-        if(point.getPointValue() - ratio < 0) throw new InsufficientPointException();
+        if(point.getPointValue() - pointRatio < 0) throw new InsufficientPointException();
 
         Stamp stamp = stampRepository.findByLabelerId(labelerId)
                 .orElseThrow(()->new NotFoundLabelerIdException());
 
-        point.decreasePoint(ratio); // 포인트 감소시킨다.
+        point.decreasePoint(pointRatio); // 포인트 감소시킨다.
         stamp.increaseStamp(1); // 개수를 1 증가시킨다.
     }
 }
