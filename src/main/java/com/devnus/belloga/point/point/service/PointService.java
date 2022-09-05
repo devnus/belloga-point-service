f69687e69ec165ed9034cfd47852e96d8e7bd865
suhongkim98 <kshz9815@naver.com>
2022-09-05 23:40:19 +0900

    feat: implement get my point, temp point info API

--- src/main/java/com/devnus/belloga/point/point/service/PointService.java
+++ src/main/java/com/devnus/belloga/point/point/service/PointService.java
@@ -1,7 +1,10 @@
 package com.devnus.belloga.point.point.service;
 
+import com.devnus.belloga.point.point.event.ResponsePoint;
+
 public interface PointService {
     void saveTempPoint(String labelerId, String labelingUUID, Long value);
     void changeTmpPointToPoint(String labelingUUID);
     void deleteTmpPoint(String labelingUUID);
+    ResponsePoint.MyPointInfo getMyPointInfo(String labelerId);
 }
