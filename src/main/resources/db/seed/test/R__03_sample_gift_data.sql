a6585a0b81981ca4e14e4184fdab395e87e84ea3
suhongkim98 <kshz9815@naver.com>
2022-11-08 21:03:10 +0900

    chore: add seed field data

--- src/main/resources/db/seed/test/R__03_sample_gift_data.sql
+++ src/main/resources/db/seed/test/R__03_sample_gift_data.sql
@@ -1,6 +1,6 @@
 /* gift */
-INSERT INTO gift(admin_id, title, gift_type, expected_draw_date) VALUES('dusik', '바나나 기프티콘 이벤트', 'GIFTICON', '2022-11-11');
-INSERT INTO gift(admin_id, title, gift_type, expected_draw_date) VALUES('dusik', '초콜릿 기프티콘 이벤트', 'GIFTICON', '2022-12-12');
+INSERT INTO gift(admin_id, title, gift_type, expected_draw_date, gift_status) VALUES('dusik', '바나나 기프티콘 이벤트', 'GIFTICON', '2022-11-11', 'WAITING');
+INSERT INTO gift(admin_id, title, gift_type, expected_draw_date, gift_status) VALUES('dusik', '초콜릿 기프티콘 이벤트', 'GIFTICON', '2022-12-12', 'WAITING');
 
 
 /* gifticon */
